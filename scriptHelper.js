3c03c9b2fc5303c1f4a34e0dc24e7852d36930c1
LaserBlaster <adamlongtutor@gmail.com>
2022-01-18 21:24:48 -0500

    updated script.js and scripthelper.js

--- scriptHelper.js
+++ scriptHelper.js
@@ -24,16 +24,48 @@ function validateInput(testInput) {
     if (isNaN(testInput.value)){
 
         alert("Enter string for pilot and co-pilot names and numbers for fuel and cargomass");
-        event.preventDefault();
      }
    
 }
 
-/* function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
+function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
+     
+      validateInput(pilot)
+      validateInput(copilot)
+      validateInput(fuelLevel)
+      validateInput(cargoLevel)
+       
+    
+     if(fuelLevel.value < 10000){
+         document.getElementById("launchStatus").innerHTML = `Shuttle not ready for launch.`
+         document.getElementById("fuelStatus").innerHTML = `There is not enough fuel for the journey.`
+         document.getElementById("launchStatus").style.color = "red"
+         document.getElementById("faultyItems").style.visibility = "visible"
+      }
+      // validate cargo conditions. And update DOM
+      if(cargoLevel.value > 10000){
+         document.getElementById("launchStatus").innerHTML = `Shuttle not ready for launch.`
+         document.getElementById("cargoStatus").innerHTML = `There is too much mass for the shuttle to take off.`;
+         document.getElementById("launchStatus").style.color = "red"
+         document.getElementById("faultyItems").style.visibility = "visible"
+      }
+      
+      
+      // validate fuel and cargo conditions. And update DOM
+      if(fuelLevel.value > 10000 && cargoLevel.value <10000){
+         document.getElementById("launchStatus").innerHTML = `Shuttle is ready for launch.`
+         document.getElementById("launchStatus").style.color = "green"
+      }
+      // update name and status
+      if (pilot.value && copilot.value){
+         document.getElementById("pilotStatus").innerHTML = `Pilot ${pilotName.value} is ready for launch`
+         document.getElementById("copilotStatus").innerHTML = `Co-pilot ${copilotName.value} is ready for launch`
+         document.getElementById("faultyItems").style.visibility = "visible"
+      }
    
 }
 
-async function myFetch() {
+/*async function myFetch() {
     let planetsReturned;
 
     planetsReturned = await fetch().then( function(response) {
@@ -44,10 +76,9 @@ async function myFetch() {
 
 function pickPlanet(planets) {
 }
-
+*/
 module.exports.addDestinationInfo = addDestinationInfo;
 module.exports.validateInput = validateInput;
 module.exports.formSubmission = formSubmission;
 module.exports.pickPlanet = pickPlanet; 
 module.exports.myFetch = myFetch;
-*/
